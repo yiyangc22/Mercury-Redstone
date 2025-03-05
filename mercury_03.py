a998af755fdeb06ce77159b63e2099c08fa44799
Yiyang (Ian) Chen <160797348+yiyangc22@users.noreply.github.com>
2025-03-05 15:44:04 -0800

    Update mercury_03.py

--- mercury_03.py
+++ mercury_03.py
@@ -13,7 +13,8 @@ WINDOW_TXT = "Mercury III - Fluid Scheme Constructor"
 WINDOW_RES = "900x100"
 PARAMS_DTP = os.path.join(os.path.expanduser("~"), "Desktop")               # desktop folder path
 PARAMS_DFT = os.path.join(PARAMS_DTP, "_latest")                            # default folder path
-PARAMS_LOG = os.path.join(PARAMS_DFT, "data.csv")
+PARAMS_LOG = os.path.join(PARAMS_DFT, "_cleave_cycle.csv")
+PARAMS_IMG = "Subgroup 1"
 
 
 # ===================================== customtkinter classes =====================================
@@ -25,7 +26,7 @@ class Moa:
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ on enable ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def __init__(self):
         super().__init__()
-        self.rtn = ([],[],[])
+        self.rtn = ([],'','',[])
 
 
 class App(customtkinter.CTk, Moa):
@@ -51,98 +52,23 @@ class App(customtkinter.CTk, Moa):
         Function: commence the export of collected user inputs.
         """
         # variable set up, read csv file
-        rtn = ([],[],[])
-        csv = get_csv(self.frm_ctl.ent_pth.get())
-        # check if there are null values in the command list
-        # then, arrange csv data into compatable format
-        for entry in csv:
-            # assume all commands are valid
-            cmd = [True, True, True]
-            # check if entry has parameters for laser procedure
-            for i in range(0, 5):
-                if entry[i] == '' or entry[i] == 'null':
-                    cmd[0] = False
-                    break
-            # check if entry has parameters for fluidic procedure
-            for i in range(5, 8):
-                if entry[i] == '' or entry[i] == 'null':
-                    cmd[1] = False
-                    break
-            # check if entry has parameters for wait procedure
-            if entry[8] == '' or entry[8] == 'null':
-                cmd[2] = False
-            # append commands into rtn in rearranged format
-            #######################################################################################
-            #######################################################################################
-            # if the row is completely filled (full experiment run), append full fluidic procedures
-            if cmd[0] and cmd[1] and cmd[2]:
-                # ---------------------------------------------------------------------------------
-                # ligase buffer wash
-                rtn[0].append(2)
-                rtn[1].append([24.0, 500.0, 500.0])
-                rtn[2].append('')
-                # ---------------------------------------------------------------------------------
-                # 2p cleave laser
-                if entry[4] == 0:
-                    rtn[0].append(0)
-                else:
-                    rtn[0].append(1)
-                rtn[1].append([float(entry[0]), float(entry[1]), float(entry[2])])
-                rtn[2].append(entry[3])
-                # ---------------------------------------------------------------------------------
-                # ligation buffer wash
-                rtn[0].append(2)
-                rtn[1].append([24.0, 500.0, 500.0])
-                rtn[2].append('')
-                # ---------------------------------------------------------------------------------
-                # ligagion reaction (active)
-                rtn[0].append(2)
-                rtn[1].append([float(entry[5]), float(entry[6]), float(entry[7])])
-                rtn[2].append('')
-                # ---------------------------------------------------------------------------------
-                # wait (ligation time)
-                rtn[0].append(3)
-                rtn[1].append([float(entry[8]), 0.0, 0.0])
-                rtn[2].append('')
-                # ---------------------------------------------------------------------------------
-                # DPBS wash
-                rtn[0].append(2)
-                rtn[1].append([23.0, 500.0, 1000.0])
-                rtn[2].append('')
-            #######################################################################################
-            #######################################################################################
-            # if a row does not include parameters needed for a full experiment run
-            else:
-                # for laser procedures
-                if cmd[0]:
-                    # append command type to rtn[0]
-                    if entry[4] == 0:
-                        rtn[0].append(0)
-                    else:
-                        rtn[0].append(1)
-                    # append laser coordinates to rtn[1]
-                    rtn[1].append([float(entry[0]), float(entry[1]), float(entry[2])])
-                    # append mask directory to rtn[2]
-                    rtn[2].append(entry[3])
-                # for fluidic procedures
-                if cmd[1]:
-                    # append command type to rtn[0]
-                    rtn[0].append(2)
-                    # append fluidic parameters to rtn[1]
-                    rtn[1].append([float(entry[5]), float(entry[6]), float(entry[7])])
-                    # append empty string to rtn[2]
-                    rtn[2].append('')
-                # for wait procedures
-                if cmd[2]:
-                    # append command type to rtn[0]
-                    rtn[0].append(3)
-                    # append wait parameter to rtn[1]
-                    rtn[1].append([float(entry[8]), 0.0, 0.0])
-                    # append empty string to rtn[2]
-                    rtn[2].append('')
+        pth = self.frm_ctl.ent_pth.get()
+        fld = os.path.join(get_rtl(pth), PARAMS_IMG)
+        csv = get_csv(pth)
+        rtn = ([],fld,pth,[])
+        # arrange csv data into compatable format
+        current_port = csv[0][4]
+        rtn[0].append(current_port)
+        rtn[3].append(0)
+        for i, entry in enumerate(csv):
+            if current_port != entry[4]:
+                current_port = entry[4]
+                rtn[3].append(i)
+                rtn[0].append(current_port)
         # return results by saving rtn
         self.rtn = rtn
         self.quit()
+
 
 class Apd(customtkinter.CTkFrame):
     """
@@ -157,7 +83,7 @@ class Apd(customtkinter.CTkFrame):
             master = self,
             width = 200,
             height = 28,
-            text = "Experiment Log File (data.csv):"
+            text = "Experiment Log File (.csv):"
         )
         self.lbl_pth.grid(row=0, column=0, padx=(10,0), pady=5, sticky="nsew")
         self.ent_pth = customtkinter.CTkEntry(
@@ -217,6 +143,25 @@ def decode_cpmask_tolist(
     return rtn
 
 
+def decode_cleave_params(
+        path,
+        starting_index
+):
+    """
+    Function: open, decode, and get cleave parameters from a specific index.
+    """
+    command_list = get_csv(path)
+    command_port = command_list[starting_index][4]
+    rtn = ([],[])
+    for i in range(starting_index, len(command_list)):
+        if command_list[i][4] == command_port:
+            rtn[0].append(command_list[i][3])
+            rtn[1].append([command_list[i][0], command_list[i][1], command_list[i][2]])
+        else:
+            return rtn
+    return rtn
+
+
 # ========================================= main function =========================================
 
 def mercury_03():
@@ -233,4 +178,4 @@ def mercury_03():
     try:
         return app.rtn
     except AttributeError:
-        return ([],[],[])
+        return ([],'','',[])
